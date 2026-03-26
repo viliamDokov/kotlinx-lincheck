ef2678e707b738a629489bde2bb60d2512a6cfd7
Wiliam Dokov <viliam.dokov@gmail.com>
2026-03-26 18:25:30 +0100

    Add different litmus test verification options

--- src/jvm/test/org/jetbrains/kotlinx/lincheck_test/strategy/eventstructure/Utils.kt
+++ src/jvm/test/org/jetbrains/kotlinx/lincheck_test/strategy/eventstructure/Utils.kt
@@ -20,6 +20,7 @@
 
 package org.jetbrains.kotlinx.lincheck_test.strategy.eventstructure
 
+import org.jetbrains.kotlinx.lincheck.*
 import org.jetbrains.kotlinx.lincheck.execution.*
 import org.jetbrains.kotlinx.lincheck.runner.LambdaResultRunner
 import org.jetbrains.kotlinx.lincheck.strategy.managed.eventstructure.*
@@ -27,6 +28,7 @@ import org.jetbrains.kotlinx.lincheck.strategy.runIteration
 import org.jetbrains.lincheck.datastructures.ModelCheckingOptions
 import org.jetbrains.lincheck.datastructures.verifier.Verifier
 import org.jetbrains.lincheck.jvm.agent.InstrumentationMode
+import org.jetbrains.lincheck.jvm.agent.withLincheckJavaAgent
 import org.jetbrains.lincheck.withLincheckTestContext
 import org.jetbrains.kotlinx.lincheck.util.*
 import org.jetbrains.lincheck.datastructures.ModelCheckingCTestConfiguration
@@ -36,14 +38,43 @@ import org.junit.Assert
 
 internal const val UNIQUE = -1
 internal const val UNKNOWN = -2
+
+
+//TODO: Maybe we can make a class out of this, instead of this function nonsense
+typealias OutcomeVerifier<Outcome> = (Set<Outcome>, Int) -> Unit
+internal fun <Outcome> assertNever(forbiddenOutcomes: Set<Outcome>): OutcomeVerifier<Outcome> = { actualOutcomes, invocations ->
+    val overlap = forbiddenOutcomes.intersect(actualOutcomes)
+    Assert.assertEquals("Forbidden outcomes detected: $overlap", overlap.size, 0)
+}
+
+internal fun <Outcome> assertSometimes(expectedOutcomes: Set<Outcome>): OutcomeVerifier<Outcome> = { actualOutcomes, invocations ->
+    val missing = expectedOutcomes - actualOutcomes
+    Assert.assertEquals("Some outcomes not detected:\n$missing\nGot:$actualOutcomes", missing.size, 0)
+}
+
+internal fun <Outcome> assertAlways(expectedOutcomes: Set<Outcome>, executionCount: Int = UNIQUE): OutcomeVerifier<Outcome> {
+    require(executionCount >= 0 || executionCount == UNIQUE || executionCount == UNKNOWN)
+    return { actualOutcomes, invocations ->
+        val missing = expectedOutcomes - actualOutcomes
+        val unexpected = actualOutcomes - expectedOutcomes
+        val msg = "Some outcomes not detected.\nMissing:$missing\nUnexpected:$unexpected:\n"
+        Assert.assertEquals(msg, expectedOutcomes, actualOutcomes)
+
+        val expectedCount = when (executionCount) {
+            UNIQUE -> expectedOutcomes.size
+            UNKNOWN -> invocations
+            else -> executionCount
+        }
+        Assert.assertEquals(expectedCount, invocations)
+    }
+}
+
 internal fun<Outcome> litmusTest(
     testClass: Class<*>,
     testScenario: ExecutionScenario,
-    expectedOutcomes: Set<Outcome>,
-    executionCount: Int = UNIQUE,
+    outcomeVerifier: OutcomeVerifier<Outcome>,
     getOutcome: (ExecutionResult) -> Outcome,
 ) {
-    require(executionCount >= 0 || executionCount == UNIQUE || executionCount == UNKNOWN)
     val outcomes: MutableSet<Outcome> = mutableSetOf()
     val verifier = createVerifier(testScenario) { results ->
         outcomes.add(getOutcome(results))
@@ -53,17 +84,7 @@ internal fun<Outcome> litmusTest(
         val strategy = createStrategy(testClass, testScenario)
         val failure = strategy.runIteration(INVOCATIONS, verifier)
         assert(failure == null) { failure.toString() }
-        val missingOutcomes = expectedOutcomes - outcomes;
-        val extraOutomes = outcomes - expectedOutcomes;
-        val message = "Litmus test outcomes are different:\nMissing $missingOutcomes\nExtra: $extraOutomes\n"
-        Assert.assertEquals(message, expectedOutcomes, outcomes)
-
-        val expectedCount = when (executionCount) {
-            UNIQUE -> expectedOutcomes.size
-            UNKNOWN -> strategy.stats.consistentInvocations
-            else -> executionCount
-        }
-        Assert.assertEquals(expectedCount, strategy.stats.consistentInvocations)
+        outcomeVerifier(outcomes, strategy.stats.consistentInvocations)
     }
 }
 
@@ -137,6 +158,7 @@ internal fun <T> createStrategy(testCfg: ModelCheckingCTestConfiguration, block:
 internal inline fun<reified Outcome> litmustTestv2 (
     expectedOutcomes: Set<Outcome>,
     executionCount: Int = UNIQUE,
+    outcomeVerifier: OutcomeVerifier<Outcome>,
     noinline block: () -> Outcome,
 ) {
     val INVOCATIONS = 10000
@@ -153,11 +175,8 @@ internal inline fun<reified Outcome> litmustTestv2 (
         createStrategy(testCfg, block).use { strategy ->
             val failure = strategy.runIteration(INVOCATIONS, verifier)
             assert(failure == null) { failure.toString() }
-            val missingOutcomes = expectedOutcomes - outcomes;
-            val extraOutomes = outcomes - expectedOutcomes;
-            val message = "Litmus test outcomes are different:\nMissing $missingOutcomes\nExtra: $extraOutomes\n"
-            Assert.assertEquals(message, expectedOutcomes, outcomes)
 
+            Assert.assertEquals(expectedOutcomes, outcomes)
             val expectedCount = when (executionCount) {
                 UNIQUE -> expectedOutcomes.size
                 UNKNOWN -> strategy.stats.consistentInvocations
