4e537ff3d33ddd780945d82d1c2fc26e49ff7d0f
Wiliam Dokov <viliam.dokov@gmail.com>
2026-06-10 00:41:00 +0200

    Add onArrayCopy transformer

--- jvm-agent/src/main/org/jetbrains/lincheck/jvm/agent/TransformationProfile.kt
+++ jvm-agent/src/main/org/jetbrains/lincheck/jvm/agent/TransformationProfile.kt
@@ -34,6 +34,7 @@ class TransformationConfiguration(
     var trackStaticFieldWrites: Boolean = false,
     var trackArrayElementReads: Boolean = false,
     var trackArrayElementWrites: Boolean = false,
+    var trackArrayCopy: Boolean = false,
 
     var trackMethodCalls: Boolean = false,
     var interceptMethodCallResults: Boolean = false,
@@ -192,6 +193,8 @@ internal fun TransformationConfiguration.shouldApplyVisitor(visitorClass: Class<
 
         ThrowTransformer::class.java -> trackThrows
         CatchBlockStartTransformer::class.java -> trackCatchBlocks
+
+        ReflectionTransformer::class.java -> trackArrayCopy
 
         // the configuration does not govern other types of transformers,
         // so they should be applied by default
@@ -590,6 +593,7 @@ object ExperimentalModelCheckingTransformationProfile : TransformationProfile {
             trackLocalVariableWrites = true
             trackArrayElementReads = true
             trackArrayElementWrites = true
+            trackArrayCopy = true
 
             interceptReadResults = true
         }
