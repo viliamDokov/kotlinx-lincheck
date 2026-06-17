e74ab49023026f4dc588b6c96092ed737bdbf7c3
Wiliam Dokov <viliam.dokov@gmail.com>
2026-06-17 17:06:49 +0200

    Revert "Enable loop detector for EventStructureStrategy.kt"
    This reverts commit 2f5e3560e258ff76321ce965a949729cb09d021f.

--- jvm-agent/src/main/org/jetbrains/lincheck/jvm/agent/TransformationProfile.kt
+++ jvm-agent/src/main/org/jetbrains/lincheck/jvm/agent/TransformationProfile.kt
@@ -599,10 +599,6 @@ object ExperimentalModelCheckingTransformationProfile : TransformationProfile {
             trackArrayCopy = true
 
             interceptReadResults = true
-
-            trackLoops = true
-            trackIrreducibleLoops = true
-            trackAwaitLoops = false // TODO: This is broken currently
         }
     }
 }
